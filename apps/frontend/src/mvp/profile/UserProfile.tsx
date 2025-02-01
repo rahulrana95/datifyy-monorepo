7cf930e0e942f7f54287ded6b6e3292027492f9e
Rahul Rana <3rahul4@gmail.com>
2025-02-01 12:55:00 +0530

    Update profile

--- apps/frontend/src/mvp/profile/UserProfile.tsx
+++ apps/frontend/src/mvp/profile/UserProfile.tsx
@@ -12,11 +12,38 @@ import {
     useTheme,
     Image,
     SimpleGrid,
+    Text,
+    Flex,
 } from "@chakra-ui/react";
+import { FaUser, FaBirthdayCake, FaEnvelope, FaCheckCircle, FaIdCard, FaPhone, FaRuler, FaDumbbell, FaBeer, FaSmoking, FaHeart, FaCity, FaHome, FaCalendarAlt, FaBaby, FaBabyCarriage, FaTransgender, FaCamera, FaEdit } from "react-icons/fa";
 import CitySelect from "./CitySelect";
-import MalePreview from '../../assets/images/male-preview.jpeg';
-import FemalePreview from '../../assets/images/female-preview.jpeg';
+import MalePreview from "../../assets/images/male-preview.jpeg";
+import FemalePreview from "../../assets/images/female-preview.jpeg";
 import ImageUploadButton from "./ImageUploadButton";
+
+
+const fieldIcons = {
+    firstName: FaUser,
+    lastName: FaUser,
+    dob: FaBirthdayCake,
+    officialEmail: FaEnvelope,
+    isOfficialEmailVerified: FaCheckCircle,
+    isAadharVerified: FaIdCard,
+    isPhoneVerified: FaPhone,
+    height: FaRuler,
+    exercise: FaDumbbell,
+    drinking: FaBeer,
+    smoking: FaSmoking,
+    lookingFor: FaHeart,
+    currentCity: FaCity,
+    hometown: FaHome,
+    settleDownInMonths: FaCalendarAlt,
+    haveKids: FaBaby,
+    wantsKids: FaBabyCarriage,
+    starSign: FaCamera,
+    pronoun: FaTransgender,
+    profileImage: FaCamera
+};
 
 const enums = {
     gender: ["Male", "Female", "Other"],
@@ -43,9 +70,10 @@ const enums = {
     pronoun: ["He/Him", "She/Her", "They/Them", "Other"],
 };
 
-interface FormField {
+export interface FormField {
     name: string;
     label: string;
+    icon?: any;
     type: "text" | "email" | "date" | "select" | "checkbox" | "city" | "image";
     options?: string[];
 }
@@ -60,13 +88,14 @@ const formConfig: FormSection[] = [
         section: "Personal Information",
         fields: [
             [
-                { name: "firstName", label: "First Name", type: "text" },
-                { name: "lastName", label: "Last Name", type: "text" },
-                { name: "dob", label: "Date of Birth", type: "date" },
+                { name: "firstName", label: "First Name", type: "text", icon: fieldIcons.firstName },
+                { name: "lastName", label: "Last Name", type: "text", icon: fieldIcons.lastName },
+                { name: "dob", label: "Date of Birth", type: "date", icon: fieldIcons.dob },
                 {
                     name: "gender",
                     label: "Gender",
                     type: "select",
+                    icon: fieldIcons.profileImage,
                     options: enums.gender,
                 },
             ],
@@ -74,27 +103,30 @@ const formConfig: FormSection[] = [
                 {
                     name: "profileImage",
                     label: "Profile Image",
+                    icon: fieldIcons.profileImage,
                     type: "image",
                 },
-            ]
+            ],
         ],
     },
     {
         section: "Contact Details",
         fields: [
             [
-                { name: "officialEmail", label: "Email", type: "email" },
+                { name: "officialEmail", label: "Email", type: "email", icon: fieldIcons.officialEmail },
                 {
                     name: "isOfficialEmailVerified",
                     label: "Email Verified",
                     type: "checkbox",
+                    icon: fieldIcons.isOfficialEmailVerified,
                 },
                 {
                     name: "isAadharVerified",
                     label: "Aadhar Verified",
                     type: "checkbox",
+                    icon: fieldIcons.isAadharVerified,
                 },
-                { name: "isPhoneVerified", label: "Phone Verified", type: "checkbox" },
+                { name: "isPhoneVerified", label: "Phone Verified", type: "checkbox", icon: fieldIcons.isPhoneVerified },
             ],
         ],
     },
@@ -107,11 +139,13 @@ const formConfig: FormSection[] = [
                     name: "exercise",
                     label: "Exercise",
                     type: "select",
+                    icon: fieldIcons.exercise,
                     options: enums.exercise,
                 },
                 {
                     name: "drinking",
                     label: "Drinking",
+                    icon: fieldIcons.drinking,
                     type: "select",
                     options: enums.drinking,
                 },
@@ -119,12 +153,14 @@ const formConfig: FormSection[] = [
                     name: "smoking",
                     label: "Smoking",
                     type: "select",
+                    icon: fieldIcons.smoking,
                     options: enums.smoking,
                 },
                 {
                     name: "lookingFor",
                     label: "Looking For",
                     type: "select",
+                    icon: fieldIcons.lookingFor,
                     options: enums.lookingFor,
                 },
             ],
@@ -134,8 +170,8 @@ const formConfig: FormSection[] = [
         section: "Location",
         fields: [
             [
-                { name: "currentCity", label: "Current City", type: "city" },
-                { name: "hometown", label: "Hometown", type: "city" },
+                { name: "currentCity", label: "Current City", type: "city", icon: fieldIcons.currentCity },
+                { name: "hometown", label: "Hometown", type: "city", icon: fieldIcons.hometown },
             ],
         ],
     },
@@ -146,21 +182,24 @@ const formConfig: FormSection[] = [
                 {
                     name: "settleDownInMonths",
                     label: "Settle Down In",
+                    icon: fieldIcons.settleDownInMonths,
                     type: "select",
                     options: enums.settleDownInMonths,
                 },
-                { name: "haveKids", label: "Have Kids", type: "checkbox" },
-                { name: "wantsKids", label: "Wants Kids", type: "checkbox" },
+                { name: "haveKids", label: "Have Kids", type: "checkbox", icon: fieldIcons.haveKids },
+                { name: "wantsKids", label: "Wants Kids", type: "checkbox", icon: fieldIcons.wantsKids },
                 {
                     name: "starSign",
                     label: "Star Sign",
                     type: "select",
+                    icon: fieldIcons.starSign,
                     options: enums.starSign,
                 },
                 {
                     name: "pronoun",
                     label: "Pronoun",
                     type: "select",
+                    icon: fieldIcons.pronoun,
                     options: enums.pronoun,
                 },
             ],
@@ -168,26 +207,41 @@ const formConfig: FormSection[] = [
     },
 ];
 
+export interface ProfileData {
+    [key: string]: string | boolean | undefined;
+}
+
 const ProfileForm = () => {
     const theme = useTheme();
-    const [profileData, setProfileData] = useState<ProfileData>({});
+    const [profileData, setProfileData] = useState<ProfileData>({
+        firstName: "John",
+        lastName: "Doe",
+        dob: "1990-01-01",
+        gender: "male",
+        officialEmail: "john.doe@example.com",
+        isOfficialEmailVerified: true,
+        isAadharVerified: true,
+        isPhoneVerified: true,
+        height: "180 cm",
+        exercise: "Moderate",
+        drinking: "Occasionally",
+        smoking: "Never",
+        lookingFor: "Relationship",
+        currentCity: "New York",
+        hometown: "Los Angeles",
+        settleDownInMonths: "12-24",
+        haveKids: false,
+        wantsKids: true,
+        starSign: "Leo",
+        pronoun: "He/Him",
+        profileImage: "",
+    });
     const [imagePreview, setImagePreview] = useState<string | null>(null);
+    const [isEditMode, setIsEditMode] = useState<{
+        [key: string]: boolean;
+    }>({});
 
-    interface FormField {
-        name: string;
-        label: string;
-        type: "text" | "email" | "date" | "select" | "checkbox" | "city" | "image";
-        options?: string[];
-    }
 
-    interface FormSection {
-        section: string;
-        fields: FormField[];
-    }
-
-    interface ProfileData {
-        [key: string]: string | boolean | undefined;
-    }
 
     const handleChange = (
         e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
@@ -207,59 +261,119 @@ const ProfileForm = () => {
         }
     };
 
-    const renderField = (field: FormField) => {
+    const renderFieldReadView = (text: string, value: string, icon: any, fieldName: string) => {
+        let newValue = value;
+        if (fieldName === "dob") {
+            const calculateAge = (dob: string) => {
+                const birthDate = new Date(value);
+                const today = new Date();
+                let age = today.getFullYear() - birthDate.getFullYear();
+                const monthDifference = today.getMonth() - birthDate.getMonth();
+                if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+                    age--;
+                }
+                return age;
+            };
+
+            newValue = `${value} (${calculateAge(value)} years)`;
+        }
+        return <Flex justifyContent="space-between" alignItems={"center"} border={"1px solid"} padding={4} pt={2} pb={2} borderColor={"gray.200"} borderRadius={8} fontSize={12} width={"100%"}>
+            <HStack>
+                <Box as={icon} mr={2} />
+                <Text>{text}</Text>
+            </HStack>
+            <Text>{newValue ?? value}</Text>
+        </Flex>
+    }
+    const renderField = (field: FormField, sectionId: string) => {
+        const isEditEnabled = isEditMode[sectionId];
         switch (field.type) {
             case "text":
             case "email":
             case "date":
                 return (
-                    <Input
-                        id={field.name}
-                        name={field.name}
-                        type={field.type}
-                        value={String(profileData[field.name] ?? "")}
-                        onChange={handleChange}
-                    />
+                    isEditEnabled ?
+                        <Input
+                            fontSize={13}
+                            id={field.name}
+                            name={field.name}
+                            type={field.type}
+                            value={String(profileData[field.name] ?? "")}
+                            onChange={handleChange}
+                            isReadOnly={!isEditEnabled}
+                        /> : renderFieldReadView(field.label, String(profileData[field.name] ?? ""), field.icon, field.name)
                 );
             case "select":
                 return (
-                    <Select
-                        id={field.name}
-                        name={field.name}
-                        value={String(profileData[field.name] ?? "")}
-                        onChange={handleChange}
-                    >
-                        {field.options?.map((option) => (
-                            <option key={option} value={option}>
-                                {option}
-                            </option>
-                        ))}
-                    </Select>
+                    isEditEnabled ?
+                        <Select
+                            id={field.name}
+                            name={field.name}
+                            value={String(profileData[field.name] ?? "")}
+                            onChange={handleChange}
+                            isReadOnly={!isEditEnabled}
+                            fontSize={13}
+                        >
+                            {field.options?.map((option) => (
+                                <option key={option} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </Select> : renderFieldReadView(field.label, String(profileData[field.name] ?? ""), field.icon, field.name)
                 );
             case "checkbox":
                 return (
-                    <Checkbox
-                        id={field.name}
-                        name={field.name}
-                        isChecked={!!profileData[field.name] || false}
-                        onChange={handleChange}
-                    >
-                        {field.label}
-                    </Checkbox>
+                    isEditEnabled ?
+                        <Checkbox
+                            id={field.name}
+                            name={field.name}
+                            isChecked={!!profileData[field.name] || false}
+                            onChange={handleChange}
+                            isDisabled={!isEditEnabled}
+                        >
+                            {field.label}
+                        </Checkbox> : renderFieldReadView(field.label, String(profileData[field.name] ?? ""), field.icon, field.name)
                 );
             case "city":
-                return <CitySelect />;
+                return isEditEnabled ? <CitySelect /> : renderFieldReadView(field.label, String(profileData[field.name] ?? ""), field.icon, field.name);
             case "image":
-                return <div className="image-upload">
-                    <Box className="image-preview" mb={6}>
-                        {!imagePreview ? <img src={MalePreview} width="200px" /> : <Image src={imagePreview ?? ''} boxSize="100px" objectFit="cover" />}
-                    </Box>
-                    <ImageUploadButton handleImageUpload={handleImageUpload} />
-                </div>
+                return (
+                    <div className="image-upload">
+                        <Box className="image-preview" mb={6}>
+                            {!imagePreview ? (
+                                <img src={MalePreview} width="200px" alt="Male Preview" />
+                            ) : (
+                                <Image
+                                    src={imagePreview ?? ""}
+                                    boxSize="100px"
+                                    objectFit="cover"
+                                />
+                            )}
+                        </Box>
+                        {isEditEnabled && (
+                            <ImageUploadButton handleImageUpload={handleImageUpload} />
+                        )}
+                    </div>
+                );
             default:
                 return null;
         }
     };
+
+    const handleSave = (sectionId: string) => {
+        setIsEditMode((isEditMode) => ({ ...isEditMode, [sectionId]: false }));
+        // Save logic goes here (e.g., send data to server)
+    };
+
+    const handleDiscard = (sectionId: string) => {
+        setIsEditMode((isEditMode) => ({ ...isEditMode, [sectionId]: false }));
+        // Discard logic goes here (e.g., revert changes)
+    };
+
+    const handleEdit = (sectionId: string) => {
+        setIsEditMode((isEditMode) => ({ ...isEditMode, [sectionId]: true }));
+
+    }
 
     return (
         <Box maxW="900px" margin="0 auto" pt={8}>
@@ -273,9 +387,13 @@ const ProfileForm = () => {
                             borderRadius={20}
                             w="full"
                         >
-                            <FormLabel fontSize="lg" fontWeight="bold">
-                                {section.section}
-                            </FormLabel>
+                            <Flex justifyContent={"space-between"} alignItems={"center"} mb={4}>
+                                <FormLabel fontSize="lg" fontWeight="bold">
+                                    {section.section}
+                                </FormLabel>
+                                {!isEditMode[section.section] && <Box as={FaEdit} color={theme.colors.accent[500]} onClick={() => handleEdit(section.section)} />}
+                            </Flex>
+
                             <SimpleGrid
                                 columns={{ base: 1, md: section.fields.length }}
                                 spacing={4}
@@ -283,14 +401,29 @@ const ProfileForm = () => {
                                 {section.fields.map((fieldCol, colIndex) => (
                                     <VStack key={colIndex} spacing={4} align="stretch">
                                         {fieldCol.map((field) => (
-                                            <FormControl key={field.name} isRequired>
-                                                <FormLabel htmlFor={field.name}>{field.label}</FormLabel>
-                                                {renderField(field)}
+                                            <FormControl key={field.name} isRequired style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+                                                {isEditMode[section.section] && ['profileImage'].indexOf(field.name) < 0 && <FormLabel htmlFor={field.name} fontSize={12} width={"40%"} >
+                                                    {field.label}
+                                                </FormLabel>
+                                                }
+                                                {
+                                                    renderField(field, section.section)
+                                                }
                                             </FormControl>
                                         ))}
+
                                     </VStack>
                                 ))}
                             </SimpleGrid>
+                            {isEditMode[section.section] && (
+                                <HStack spacing={4} mt={6}>
+                                    <Button onClick={() => handleSave(section.section)} size={"sm"} bg={theme.colors.accent[500]} color="white">Save</Button>
+                                    <Button onClick={() => handleDiscard(section.section)} size={"sm"} bg={theme.colors.accent[800]} colorScheme="red">
+                                        Discard
+                                    </Button>
+                                </HStack>
+                            )}
+
                         </Box>
                     ))}
                 </VStack>
